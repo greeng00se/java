1796208c7705d70731404afa315e1e923b3069f7
greeng00se <thegreengoose7@gmail.com>
2022-08-20 18:46:32 +0900

    Hello Security
    - https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

---
+++ jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+// 기본적인 웹 보안 활성화
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**"
+                , "/favicon.ico"
+                , "/error");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+
+        return httpSecurity.build();
+    }
+}
