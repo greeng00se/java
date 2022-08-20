364075e867f64b7038aba4de46b3cc05fa92f137
greeng00se <thegreengoose7@gmail.com>
2022-08-20 22:26:24 +0900

    [JWT] TokenProvider, JwtFilter를 Security 로직에 등록하는 역할을 하는 JwtSecurityConfig 클래스

---
+++ jwt/src/main/java/com/example/jwt/jwt/JwtSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.jwt.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * SecurityConfigurerAdapter extends
+ * TokenProvider 를 주입받음
+ * JwtFilter 를 통해 Security 로직에 등록하는 역할을 한다.
+ */
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private TokenProvider tokenProvider;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) {
+        http.addFilterBefore(
+                new JwtFilter(tokenProvider),
+                UsernamePasswordAuthenticationFilter.class
+        );
+    }
+}
