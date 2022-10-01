aadb57602c41441b5da98f6198284c9ab8049a10
greeng00se <thegreengoose7@gmail.com>
2022-10-01 23:09:52 +0900

    Mockito UserControllerTest With WebMvcTest

---
+++ test/src/test/java/com/example/test/user/UserControllerWebMvcTest.java
@@ -0,0 +1,84 @@
+package com.example.test.user;
+
+import com.example.test.user.dto.UserRequestDto;
+import com.example.test.user.dto.UserResponseDto;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerWebMvcTest {
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("회원 가입 성공")
+    void signUpSuccess() throws Exception {
+        // given
+        UserRequestDto request = UserRequestDto.builder()
+                .name("hello")
+                .age(10L)
+                .build();
+        Mockito.doReturn(1L).when(userService)
+                .signUp(Mockito.any(UserRequestDto.class));
+
+        String json = new Gson().toJson(request);
+
+        // when
+        ResultActions result = mockMvc.perform(
+                MockMvcRequestBuilders.post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        );
+
+        // then
+        result.andExpect(status().isCreated())
+                .andExpect(content().bytes("1".getBytes(StandardCharsets.UTF_8)));
+    }
+
+    @Test
+    @DisplayName("사용자 전체 조회")
+    void findAll() throws Exception {
+        // given
+        List<UserResponseDto> users = LongStream.range(0, 5)
+                .mapToObj(i -> UserResponseDto.builder()
+                        .id(i)
+                        .name("foo" + i)
+                        .age(i)
+                        .build())
+                .collect(Collectors.toList());
+
+        Mockito.doReturn(users).when(userService)
+                .findAll();
+
+        // expected
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("foo0"))
+                .andExpect(jsonPath("$[4].name").value("foo4"))
+                .andDo(print());
+    }
+}
+
